3167bf367bdf4efb3804ee3015332a393faf2cc5
JOSE WELIFF FERREIRA LIMA <weliff.14@gmail.com>
2016-05-01 10:45:59 -0300

    Initial commit.

---
+++ src/main/java/com/algaworks/controller/CobrancaController.java
@@ -0,0 +1,42 @@
+package com.algaworks.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.model.StatusTitulo;
+import com.algaworks.model.Titulo;
+import com.algaworks.repository.Titulos;
+
+@Controller
+@RequestMapping("/titulos")
+public class CobrancaController {
+	
+	@Autowired
+	private Titulos titulos;
+	
+	@RequestMapping("/novo")
+	public ModelAndView novoTitulo(){
+		ModelAndView mv = new ModelAndView("CobrancaTitulo");
+		return mv;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);
+		ModelAndView mv = new ModelAndView("CobrancaTitulo");
+		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
+}

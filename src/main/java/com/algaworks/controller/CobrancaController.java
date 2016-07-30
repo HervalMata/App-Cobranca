5c18a7163f41b02da99b1c1b8c508be755a20ba1
Weliff Lima <weliff.14@gmail.com>
2016-07-30 16:50:41 -0300

    Adicionado validações com Thymeleaf.

--- src/main/java/com/algaworks/controller/CobrancaController.java
+++ src/main/java/com/algaworks/controller/CobrancaController.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.model.StatusTitulo;
 import com.algaworks.model.Titulo;
@@ -24,15 +27,19 @@ public class CobrancaController {
 	@RequestMapping("/novo")
 	public ModelAndView novoTitulo(){
 		ModelAndView mv = new ModelAndView("CobrancaTitulo");
+		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if (errors.hasErrors()) {
+			return "CobrancaTitulo";
+		}
 		titulos.save(titulo);
-		ModelAndView mv = new ModelAndView("CobrancaTitulo");
-		mv.addObject("mensagem", "Titulo salvo com sucesso!");
-		return mv;
+//		mv.addObject("titulo", new Titulo()); // zerar os campos do form
+		attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping
